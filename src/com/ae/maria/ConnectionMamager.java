91d2f9a433b933d3931e3dabd3a379ca09c1f8c2
AbbeyLiu <at0910392055@gmail.com>
2016-04-19 11:23:58 +0800

    abbey first git commid

--- src/com/ae/maria/ConnectionMamager.java
+++ src/com/ae/maria/ConnectionMamager.java
@@ -8,6 +8,7 @@ import java.sql.SQLException;
 
 public class ConnectionMamager {
 	
+	public static void main(String args[]){
     Connection conn=null;
     PreparedStatement pstmt=null ;
     ResultSet rs= null;
